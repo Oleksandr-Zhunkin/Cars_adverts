9881a28690c491380789f5e55183b700608b4eea
Oleksandr_Zhynkin <godinsighted@gmail.com>
2024-07-27 06:57:24 +0300

    react modal fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./index.scss";
@@ -6,6 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import ReactModal from "react-modal";
+
+ReactModal.setAppElement("#root");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
